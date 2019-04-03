f94d5d7e51db17ee5d585606165064421f3f793e
Raphael Michel <mail@raphaelmichel.de>
2019-04-03 12:45:04 +0200

    Bump to 1.5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-passbook',
-    version='1.4.0',
+    version='1.5.0',
     description='Passbook tickets for pretix',
     long_description=long_description,
     url='https://github.com/pretix/pretix-passbook',
